d706b2de2cdd50b188b10f861b89f92f73b80242
dafnevk <d.vankuppevelt@esciencecenter.nl>
2017-02-10 10:40:48 +0100

    Fixed the import (not very pretty)

--- scripts/populate_blazegraph.py
+++ scripts/populate_blazegraph.py
@@ -10,7 +10,7 @@ b : batch size
 
 import sys
 import os
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
 from rechtspraak_parser import populate_blazegraph
 
 if __name__ == "__main__":
