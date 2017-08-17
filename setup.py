fcd9680fc99c3dad25a666d7a019c79504fe23c0
dafnevk <d.vankuppevelt@esciencecenter.nl>
2017-08-17 16:48:34 +0200

    Updated version number to 0.3.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('requirements.txt') as f:
 
 setup(
     name = "caselawnet",
-    version = "0.3.1",
+    version = "0.3.2",
     description = ("Flask application for querying networks of Dutch case law"),
     license = "Apache 2.0",
     keywords = "Python",
